953d6c2cc395e89a54cc6227436a3202b16926f0
yasmin401 <Yasminm2004@gmail.com>
2025-09-29 01:05:47 +0300

    Create notes item widget ,add poppins font

---
+++ lib/widgets/notes_item.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class NotesItem extends StatelessWidget {
+  const NotesItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(top: 24, left: 16, bottom: 24),
+      decoration: BoxDecoration(
+        color: Color(0xffFFCC80),
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          ListTile(
+            title: Text(
+              'Flutter Tips',
+              style: TextStyle(color: Colors.black, fontSize: 26),
+            ),
+
+            subtitle: Padding(
+              padding: const EdgeInsets.only(bottom: 16, top: 16),
+              child: Text(
+                'Build Your Career',
+                style: TextStyle(
+                  color: Colors.black.withValues(alpha: 0.5),
+                  fontSize: 18,
+                ),
+              ),
+            ),
+            trailing: IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.delete, color: Colors.black, size: 30),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 24),
+            child: Text(
+              '29/9/2025',
+              style: TextStyle(
+                color: Colors.black.withValues(alpha: 0.4),
+                fontSize: 16,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/widgets/notes_view_body.dart
+++ lib/widgets/notes_view_body.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/widgets/custom_app_bar.dart';
+import 'package:notes_app/widgets/notes_item.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column(children: [SizedBox(height: 40), CustomAppBar()]);
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24),
+      child: const Column(
+        children: [SizedBox(height: 40), CustomAppBar(), NotesItem()],
+      ),
+    );
   }
 }
