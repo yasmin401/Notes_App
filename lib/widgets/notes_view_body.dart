d97b6eb7d50f88074a7e8a1d20bcf129c478c351
yasmin401 <Yasminm2004@gmail.com>
2025-09-28 23:35:46 +0300

    create custom app bar with custom search icon

--- lib/widgets/notes_view_body.dart
+++ lib/widgets/notes_view_body.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/widgets/custom_app_bar.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [
-        
-      ],
-    );
+    return const Column(children: [SizedBox(height: 40), CustomAppBar()]);
   }
 }
