396109e2875daa990a4d62b24d94a9a47abae21c
yasmin401 <Yasminm2004@gmail.com>
2025-10-03 19:10:03 +0300

    Add show modal bottom sheet

---
+++ lib/widgets/note_bottom_sheet.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NoteBottomSheet extends StatelessWidget {
+  const NoteBottomSheet({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
