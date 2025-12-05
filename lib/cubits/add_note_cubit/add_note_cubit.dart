7f25f47a9018f9cf7d367412bfe9aa861ece3492
yasmin401 <Yasminm2004@gmail.com>
2025-12-06 01:41:13 +0200

    create add note cubit

---
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -0,0 +1,9 @@
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+part 'add_note_state.dart';
+
+class AddNoteCubit extends Cubit<AddNoteState> {
+  AddNoteCubit() : super(AddNoteInitial());
+
+}
